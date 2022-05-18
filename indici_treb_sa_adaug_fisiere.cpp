f9e8c0a3e4c328edb9ad0e77fcb01b0a4d2e9c83
ilincafarima <41265346+ilincafarima@users.noreply.github.com>
2022-05-18 12:56:08 +0300

    Create indici_treb_sa_adaug_fisiere.cpp

---
+++ indici_treb_sa_adaug_fisiere.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+int maxNum(int x){
+int max = 0;
+while ( x > 0 )
+{
+    int remainder = x % 10;
+    if ( remainder > max )
+        max = remainder;
+
+    x /= 10;
+}
+    return max;
+}
+int main(){
+
+cout<< maxNum(12);
+}
